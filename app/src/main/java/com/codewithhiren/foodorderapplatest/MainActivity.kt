da74e13887d12e6e9bd9ef84ede67d315787ff6a
Hiren <chauhanhirenec1@gmail.com>
2025-05-22 19:54:04 +0530

    change log to main 3

--- app/src/main/java/com/codewithhiren/foodorderapplatest/MainActivity.kt
+++ app/src/main/java/com/codewithhiren/foodorderapplatest/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        Log.d("chauhan","main")
+        Log.d("chauhan","main 3")
 
         setSupportActionBar(binding.toolbar)
 
